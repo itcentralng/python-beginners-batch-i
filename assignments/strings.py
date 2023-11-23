070cb8768d8e7af86797392f29edafbf7b37691b
Nasir Mustapha <teboy.com@gmail.com>
2023-11-23 10:33:22 +0100

    string methods assignment

---
+++ assignments/strings.py
@@ -0,0 +1,4 @@
+# Q1. Practice all the methods treated today
+# Q2. Explain how the following string methods work, with an example:
+# - lower, replace, isupper, title, endswith, find, index, isalnum
+# - join, split
