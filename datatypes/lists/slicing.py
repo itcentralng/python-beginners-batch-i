8157e6f5a11a9b71655b49ac6e95912bde4c218c
Nasir Mustapha <teboy.com@gmail.com>
2023-11-28 09:46:07 +0100

    intro to list slicing

---
+++ datatypes/lists/slicing.py
@@ -0,0 +1,10 @@
+people = ["Mukhtar", "Tariq", "Maryam"]
+print(people[:2])
+print(people[:])
+print(people[1:])
+print(people[2:])
+print(people[0])
+print(people[-1])
+print(people[::1])
+print(people[::2])
+print(people[::-1])
