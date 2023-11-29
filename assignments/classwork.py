a0722db94013f55a54fd62f8ab602a5e596a37be
Nasir Mustapha <teboy.com@gmail.com>
2023-11-29 09:31:31 +0100

    datatypes summary classwork

---
+++ assignments/classwork.py
@@ -0,0 +1,13 @@
+# Q1. Create a list of 5 students
+# Q2. Using slicing, get the first two students
+# Q3. Using slicing, get the last two students
+# Q4. Create 2 variables holding a name each
+# Q5. Using the right method, add the value of each of the variables to the list above
+# Q6. Create an empty dictionary called student
+# Q7. Using the right method, remove the first student from the list above and add it
+# as a value in the above dictionary
+# Q8. Remove the last two items from the above list and create a tuple with the values
+# Q9. Add age, gender, hieght of the student into the student dictionary above using
+# the most appropraite data types.
+# Q10. Store a value for the student in the dictionary that indicates whether the student
+# is admitted or not using a boolean.
