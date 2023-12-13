a537ea3641684bac3e606d7707d088975f4ead99
Kabiru Abdullahi <yarosonyaro@gmail.com>
2023-12-13 19:29:19 +0100

    First_assignment

---
+++ kabir/assignment1.py
@@ -0,0 +1,17 @@
+
+#assignment1 = 'programming'
+#Get a list of 10 apps built with python
+
+#dropbox
+#instagram
+#facebook
+#netflix
+#spotify
+#pinterest
+#reddit
+#disqus
+#uber
+#quora
+
+
+
---
+++ kabir/assignment2.py
@@ -0,0 +1,17 @@
+#assignment2= 'variables' 
+#1 - Write 5 examples of valid variables
+name = 'kabiru'
+age = 12
+middle_name = 'abubakar'
+address = 'kaduna'
+height = 1.70
+
+#2 - Write 5 examples of invalid variables
+1name = moses
+$age = 13
+3address = kaduna
+5height = 1.80
+`middlename` = abubakar
+
+
+
