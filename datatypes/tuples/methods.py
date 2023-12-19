94f9f5b72c8fbf68fcb156c52ca263d836f3103d
Nasir Mustapha <teboy.com@gmail.com>
2023-12-19 12:53:27 +0100

    introduction to tuple and methods

---
+++ datatypes/tuples/methods.py
@@ -0,0 +1,5 @@
+items = (1, True, 1.2, "Bread", ["Purple", "Cream", "Bread", 1.2, 1.2])
+print(items.count('Bread'))
+print(items.count(1.2))
+
+# Q1. Count the number of occurances of the 1.2s in the list within the tuple above
---
+++ datatypes/tuples/slicing.py
@@ -0,0 +1,8 @@
+items = (1, 2.1, True, "Bread", ["Cake", "IceCream"], ("Person", "People"))
+print(items)
+
+print(items[4])
+print(items[5])
+print(items[2])
+print(items[:3])
+print(items[::-1])
