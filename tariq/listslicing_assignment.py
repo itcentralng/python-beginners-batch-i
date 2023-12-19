e3768bfaf16fbd4c8ed4fc583f3c744e6f76830d
Tariq Muhammad <tariqmuhd93@gmail.com>
2023-12-19 09:29:03 +0100

    List and Slicing Assignment

---
+++ tariq/listslicing_assignment.py
@@ -0,0 +1,17 @@
+# Q1. Create a list of 5 names
+# Q2. Get the second name using slicing
+# Q3. Convert the second name to uppercase
+
+# Q1. Create a list of 5 names
+names = ["Alice", "Bob", "Charlie", "David", "Eva"]
+
+# Q2. Get the second name using slicing
+second_name = names[1]
+print(second_name)
+
+# Q3. Convert the second name to uppercase
+second_name_uppercase = second_name.upper()
+
+print("List of names:", names)
+print("Second name:", second_name)
+print("Second name in uppercase:", second_name_uppercase)
