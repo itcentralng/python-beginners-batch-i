d9fbac63b6c1f071ffc2d8c6599ddeb1130fa816
Kabiru Abdullahi <yarosonyaro@gmail.com>
2023-12-21 09:55:16 +0100

    assignment-operators

---
+++ kabir/assignment-operators.py
@@ -0,0 +1,29 @@
+# Q1. Create a list of fruits
+fruits = ['orange', 'apple', 'banana', 'guava']
+print(fruits)
+
+# Q2. Using the right operator check if your
+# list contains 'apple'. Your answer should be
+# valid regardless of casing.
+print('apple' in fruits)
+
+# Q3. Remove the last item from the list and
+# check if it contains the alphabet 'a'
+print(fruits[-1])
+print('a' in fruits[-1])
+
+# Q4. Create a variable to hold the value 5
+num = 5
+print(num)
+
+# Q6. Add 5 to the above variable
+num +=5
+print(num)
+
+# Q7. Multply it by 6
+num *=6
+print(num)
+
+# Q8. Check if the final value is less than 100 or 
+# if the list of fruits doesnt contain banana
+print(num < 100 or 'banana' not in fruits)
